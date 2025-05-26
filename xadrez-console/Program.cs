9e72b7ca591cc7ed08fddc526ea080df301dc5c4
Vinícius Mota <motaviny140@gmail.com>
2025-05-26 12:24:26 -0300

    feat: begin setting pieces on the board

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez;
 
 namespace xadrez_console
 {
@@ -8,8 +9,11 @@ namespace xadrez_console
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tela.imprimirTabuleiro(tab);
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
+            Tela.imprimirTabuleiro(tab);
             Console.ReadLine();
         }
     }
