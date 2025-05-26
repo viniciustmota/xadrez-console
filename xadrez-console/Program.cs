9a291546834742ca3b645fc98b1e9308271ac255
Vinícius Mota <motaviny140@gmail.com>
2025-05-26 12:05:24 -0300

    feat: begin implementing a method to print the board

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,6 +7,9 @@ namespace xadrez_console
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
             Console.ReadLine();
         }
     }
