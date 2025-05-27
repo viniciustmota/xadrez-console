87704590ace4ceda633941e7ce91163fd518bc8c
Vinícius Mota <Vincius Mota>
2025-05-27 09:39:27 -0300

    feat: implement first unit tests for chess piece movement

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -9,15 +9,24 @@ namespace xadrez_console
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                while (!partida.Terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.Tab);
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
 
-                Tela.imprimirTabuleiro(tab);
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
+                Tela.imprimirTabuleiro(partida.Tab);
             }catch(TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
---
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,51 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    internal class PartidaDeXadrez
+    {
+        public Tabuleiro Tab { get; private set; }
+        private int Turno;
+        private Cor JogadorAtual;
+        public bool Terminada { get; private set; }
+
+        public PartidaDeXadrez()
+        {
+            Tab = new Tabuleiro(8,8);
+            Turno = 1;
+            JogadorAtual = Cor.Branca;
+            colocarPecas();
+            Terminada = false;
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = Tab.retirarPeca(origem);
+            p.incrementarQteMovimentos();
+            Peca pecaCapturada = Tab.retirarPeca(destino);
+            Tab.colocarPeca(p, destino);
+        }
+
+        private void colocarPecas()
+        {
+            Tab.colocarPeca(new Torre(Tab, Cor.Branca), new PosicaoXadrez('c', 1).toPosicao());
+            Tab.colocarPeca(new Torre(Tab, Cor.Branca), new PosicaoXadrez('c', 2).toPosicao());
+            Tab.colocarPeca(new Torre(Tab, Cor.Branca), new PosicaoXadrez('d', 2).toPosicao());
+            Tab.colocarPeca(new Torre(Tab, Cor.Branca), new PosicaoXadrez('e', 2).toPosicao());
+            Tab.colocarPeca(new Torre(Tab, Cor.Branca), new PosicaoXadrez('e', 1).toPosicao());
+            Tab.colocarPeca(new Rei(Tab, Cor.Branca), new PosicaoXadrez('d', 1).toPosicao());
+
+            Tab.colocarPeca(new Torre(Tab, Cor.Preta), new PosicaoXadrez('c', 7).toPosicao());
+            Tab.colocarPeca(new Torre(Tab, Cor.Preta), new PosicaoXadrez('c', 8).toPosicao());
+            Tab.colocarPeca(new Torre(Tab, Cor.Preta), new PosicaoXadrez('d', 7).toPosicao());
+            Tab.colocarPeca(new Torre(Tab, Cor.Preta), new PosicaoXadrez('e', 7).toPosicao());
+            Tab.colocarPeca(new Torre(Tab, Cor.Preta), new PosicaoXadrez('e', 8).toPosicao());
+            Tab.colocarPeca(new Rei(Tab, Cor.Preta), new PosicaoXadrez('d', 8).toPosicao());
+
+
+
+
+
+        }
+    }
+}
